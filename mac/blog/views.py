6bf290415a3af9b4ef27399b4dfa6652f203c429
Yash <yashkhonde222@gmail.com>
2024-04-08 17:08:21 +0530

    Added index.html in blog and shop template folder and rendered iy through views.py of both

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("In Blog Home Page")
+    return render(request, "blog/index.html")
