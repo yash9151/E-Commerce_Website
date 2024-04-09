dfa9fbefba0968c98d18c149c960f5178dcf158e
Yash <yashkhonde222@gmail.com>
2024-04-09 15:15:53 +0530

    Created pipeline of urls.py and views.py for shop app?

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="ShopHome"),
+    path('about/', views.about, name="AboutUs"),
+    path('contact', views.contact, name="ContactUs"),
+    path('tracker', views.tracker, name="TrackingStatus"),
+    path('search', views.search, name="Search"),
+    path('productview', views.prodView, name="Search"),
+    path('checkout', views.checkout, name="Checkout"),
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "shop/index.html")
+
+def about(request):
+    return HttpResponse("We are in about page of shop")
+
+def contact(request):
+    return HttpResponse("We are in contact page of shop")
+
+def tracker(request):
+    return HttpResponse("We are in tracker page of shop")
+
+def search(request):
+    return HttpResponse("We are in search page of shop")
+
+def prodView(request):
+    return HttpResponse("We are in productview page of shop")
+
+def checkout(request):
+    return HttpResponse("We are in checkout page of shop")
