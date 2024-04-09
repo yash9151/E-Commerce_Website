9029b53a6c2caa763317a4e68ca90770afaa765b
Yash <yashkhonde222@gmail.com>
2024-04-09 15:37:43 +0530

    Added Product class in Models.py and changed settings.py to navigate as shop.apps.ShopConfig and buf fix in shop\urls.py

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'shop'
+    'shop.apps.ShopConfig'
 ]
 
 MIDDLEWARE = [
--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('contact', views.contact, name="ContactUs"),
     path('tracker', views.tracker, name="TrackingStatus"),
     path('search', views.search, name="Search"),
-    path('productview', views.prodView, name="Search"),
+    path('productview', views.prodView, name="ProductView"),
     path('checkout', views.checkout, name="Checkout"),
 ]
