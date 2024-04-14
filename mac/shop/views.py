46e00d4b5c395e7911a7531b213fd1157f88e6cd
Yash <yashkhonde222@gmail.com>
2024-04-14 16:47:01 +0530

    Added Multiple Sliders and passing list of products from views.p to diplay on index.html page

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -10,7 +10,11 @@ def index(request):
     print(products)
     n=len(products)
     nSlides = n//4 + ceil((n/4) - (n//4))
-    params = {'product' : products, 'no_of_slides':nSlides, 'range' : range(1, nSlides)}
+    # params = {'product' : products, 'no_of_slides':nSlides, 'range' : range(1, nSlides)}
+
+    allProds = [[products, range(1, nSlides), nSlides],
+                [products, range(1, nSlides), nSlides]]
+    params = {'allProds' : allProds}
     return render(request, "shop/index.html", params)
 
 def about(request):
