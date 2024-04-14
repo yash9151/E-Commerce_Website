0866102a30c11388c3e02a3bd7d547e641d6d615
Yash <yashkhonde222@gmail.com>
2024-04-14 15:35:13 +0530

    Added formula in views.py of shop app and edited index.html to render all products from database to webpage Version 1

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 # Create your views here.
 def index(request):
-    return render(request, "shop/index.html")
+    # fetches products from database
+    products = Product.objects.all()
+    print(products)
+    n=len(products)
+    nSlides = n//4 + ceil((n/4) - (n//4))
+    params = {'product' : products, 'no_of_slides':nSlides, 'range' : range(1, nSlides)}
+    return render(request, "shop/index.html", params)
 
 def about(request):
     return render(request, "shop/about.html")
