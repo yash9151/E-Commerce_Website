0a545b9493a783415275756c3d1665676e046c42
Yash <yashkhonde222@gmail.com>
2024-04-28 16:30:23 +0530

    Added Category-wise division base on product categories on home page

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -11,9 +11,19 @@ def index(request):
     n=len(products)
     nSlides = n//4 + ceil((n/4) - (n//4))
     # params = {'product' : products, 'no_of_slides':nSlides, 'range' : range(1, nSlides)}
+    #
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
 
-    allProds = [[products, range(1, nSlides), nSlides],
-                [products, range(1, nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) - (n//4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
     params = {'allProds' : allProds}
     return render(request, "shop/index.html", params)
 
