5cc62a6c7ab47a99b5a15bad429b8c13bd8b96d4
Yash <yashkhonde222@gmail.com>
2024-05-12 15:25:23 +0530

    Created OrderUpdate class in models.py and registered it in admin.py and made migrations

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -39,3 +39,13 @@ class Orders(models.Model):
 
     def __str__(self):
         return self.name
+
+# to track the order we wrote below class
+class OrderUpdate(models.Model):
+    update_id= models.AutoField(primary_key=True)
+    order_id= models.IntegerField(default="")
+    update_desc= models.CharField(max_length=5000)
+    timestamp= models.DateField(auto_now_add= True)
+
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
