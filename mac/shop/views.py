43167350408538993a3e10005f4a0cd0a99386a6
Yash <yashkhonde222@gmail.com>
2024-05-12 15:49:27 +0530

    Added tracker.html and created tracker method in views.py of shop app

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
+import json
 
 # Create your views here.
 def index(request):
@@ -41,7 +42,24 @@ def contact(request):
     return render(request, "shop/contact.html")
 
 def tracker(request):
-    return render(request, "shop/tracker.html")
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
+    return render(request, 'shop/tracker.html')
 
 def search(request):
     return render(request, "shop/search.html")
@@ -66,6 +84,8 @@ def checkout(request):
 
         order = Orders(name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone, items_json=items_json)
         order.save()
+        update = OrderUpdate(order_id = order.order_id, update_desc="The order has been placed")
+        update.save()
         thank = True
         id = order.order_id
         return render(request, "shop/checkout.html", {'thank':thank, 'id':id})
