d61e556e092b2777b3e9c6bf8ef8f523fda4f072
Yash <yashkhonde222@gmail.com>
2024-05-29 19:51:17 +0530

    added blogpost.html  and basic.html page also added blogpost redirect from urls.py  and method in views.py

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def blogpost(request):
+    return render(request, "blog/blogpost.html")
